64a72b13fe7783e7a355184b6819c2880aad4636
Olga Yakimova <oa.yakimova.1969@gmail.com>
2025-03-06 19:13:29 +0300

    fixes for feature/homework

--- main.py
+++ main.py
@@ -17,3 +17,5 @@ print(sort_by_date(transactions, reverse=True))
 #print(type({'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}))
 
 # изменения
+
+# изменения feature/homework
